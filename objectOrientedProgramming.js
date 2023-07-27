4f7ee3174d7f53ae31bc2396786deafd8b391f7d
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-07-27 17:21:46 -0400

    Refactor transaction calculation logic
    - Extracted transaction calculation into a separate function
    - Improved code readability and maintainability

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -1,28 +1,34 @@
+class Transaction {
+  constructor(earnedAmount, savingsAmount) {
+    this.earnedAmount = earnedAmount;
+    this.savingsAmount = savingsAmount;
+  }
+}
+
 class TransactionManager {
-    constructor() {
-      this.transactions = {};
-    }
-  
-    calculateTotalSavingAmount(dateOfTransaction, totalEarnings, totalExpenses) {
-      const savings = totalEarnings - totalExpenses;
-      const transaction = { earnedAmount: totalEarnings, savingsAmount: savings };
-      this.transactions[dateOfTransaction] = transaction;
-      return this.transactions;
-    }
-  
-    amountForAccount(dateOfTransaction) {
-      const totalSavedOnDate = this.transactions[dateOfTransaction]?.savingsAmount;
-      if (totalSavedOnDate) {
-        const dollarSplit = totalSavedOnDate / 3;
-        return `You need to spend each of your saving accounts $${dollarSplit}`;
-      } else {
-        return `No transaction found for date ${dateOfTransaction}`;
-      }
+  constructor() {
+    this.transactions = {};
+  }
+
+  calculateTotalSavingAmount(dateOfTransaction, totalEarnings, totalExpenses) {
+    const savings = totalEarnings - totalExpenses;
+    const transaction = new Transaction(totalEarnings, savings);
+    this.transactions[dateOfTransaction] = transaction;
+    return this.transactions;
+  }
+
+  amountForAccount(dateOfTransaction) {
+    const transaction = this.transactions[dateOfTransaction];
+    if (transaction) {
+      const dollarSplit = transaction.savingsAmount / 3;
+      return `You need to spend each of your saving accounts $${dollarSplit}`;
+    } else {
+      return `No transaction found for date ${dateOfTransaction}`;
     }
   }
-  
-  // Usage:
-  const transactionManager = new TransactionManager();
-  transactionManager.calculateTotalSavingAmount('2021-10-15', 1000, 500);
-  console.log(transactionManager.amountForAccount('2021-10-15'));
-  
+}
+
+// Usage:
+const transactionManager = new TransactionManager();
+transactionManager.calculateTotalSavingAmount('2021-10-15', 1000, 500);
+console.log(transactionManager.amountForAccount('2021-10-15'));
